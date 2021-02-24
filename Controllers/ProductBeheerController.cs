08cf4e2c597bf8dde3a877f55228cb93bcb11a46
Ward Vercruyssen <r0435477@student.vives.be>
2021-02-24 17:24:12 +0100

    Product beheer scherm toegevoegd met lijst van huidige shopitems

---
+++ Controllers/ProductBeheerController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vives_FrietShop.Core;
+
+namespace Vives_FrietShop.Controllers
+{
+    public class ProductBeheerController : Controller
+    {
+        private readonly IDatabase _database;
+
+        public ProductBeheerController(IDatabase database)
+        {
+            _database = database;
+        }
+        
+        public IActionResult Index()
+        {
+            return View(_database.ShopItems);
+        }
+        
+        
+    }
+}
